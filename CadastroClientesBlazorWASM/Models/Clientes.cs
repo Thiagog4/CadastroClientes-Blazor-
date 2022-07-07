d7477b43c423639e5b4c9859683f94b78d286ceb
Thiagog4 <thiagojoseguedes@hotmail.com>
2022-07-07 15:56:47 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroClientesBlazorWASM/Models/Clientes.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroClientesBlazorWASM.Models
+{
+    public class Clientes
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage ="CPF obrigatorio")]
+        [StringLength(11)]
+        public string Cpf { get; set; }
+
+
+        [Required(ErrorMessage = "Nome obrigatorio")]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "Celular obrigatorio")]
+        public string Celular { get; set; }
+
+
+        [Required(ErrorMessage = "CEP obrigatorio")]
+        public string? Cep { get; set; }
+
+
+        [Required(ErrorMessage = "Endereço obrigatorio")]
+        public string Endereco { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Email { get; set; }
+        public string? EstadoCivil { get; set; }
+
+    }
+}
