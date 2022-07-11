938269835e9551c8a7ef3de322332128c3dba89b
Thiagog4 <thiagojoseguedes@hotmail.com>
2022-07-11 01:24:29 -0300

    Alterado tela de contatos

---
+++ CadastroClientesBlazorWASM/Models/ContatosModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CadastroClientesBlazorWASM.Models
+{
+    public class ContatosModel
+    {
+
+        public int Id { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Celular { get; set; }
+
+    }
+}
